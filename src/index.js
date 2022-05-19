c82cf7c908b3595140ccdb38c5ff70a29a9c632a
AnamariaMC <anymedinac14@gmail.com>
2022-05-18 21:21:42 -0500

    Estilos de la vista login e integración de formulario con los mensajes de error para el inicio de seción

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-//-------- importamos las vistas -------- :apuntando_hacia_abajo:
+import Routes from './routes';
 
-import Login from './pages/login';
-import Chef from './pages/chef';
-import Waiter from './pages/waiter';
-import Admin  from './pages/admin';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <BrowserRouter>
-        <Routes>
-            
-            <Route path='/' element={<Login/>}></Route>
-            <Route path='waiter' element={<Waiter/>}></Route>
-            <Route path='chef' element={<Chef/>}></Route>
-            <Route path='admin' element={<Admin/>}></Route>
-            <Route path='*' element={<Navigate to={"/"} />}></Route>
-        </Routes>
-    </BrowserRouter>
-)
+    root.render(
+        <React.StrictMode>
+            <Routes />
+        </React.StrictMode>
+    );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,76 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Formik } from 'formik'
+import hamburger from '../lib/hamburger.png'
+import '../login.css'
 
-export default function Login() {
-  return (
-    <div>
-      <h1>Hola, Bienvenido al login</h1>
-      <p>
-      <Link to={'Waiter'}>Waiter</Link>
-      </p>
-      <p>
-      <Link to={'Chef'}>Chef</Link>
-      </p>
-      <p>
-      <Link to={'Admin'}>Admin</Link>
-      </p>
-    </div>
-  )
+const Formulario = () => {
+	return (      
+		<div className='contenedor'>
+      <img className='hamburger' src={hamburger} alt="hamburger" />
+      <Formik
+        initialValues={{
+          email:'',
+          password:''
+        }}
+        validate={(valores) => {
+          let errores = {};
+
+          //validación email
+          if(!valores.email){
+            errores.email = 'Por favor ingresa un email'
+          } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)){
+              errores.email = 'El correo es incorrecto'
+          }
+          //validación contraseña
+          if(!valores.password){
+            errores.password = 'Por favor ingresa una contraseña'
+          } else if (!/^(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/
+          .test(valores.password)){
+              errores.password = 'Debe tener minimo 6 letras y almenos un numero'
+          }
+          
+          return errores;
+        }}
+        onSubmit={(valores) => {
+          console.log(valores);
+          console.log('Formulario enviado');
+        }}
+      >
+        {({values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+          <form className="formulario" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Correo</label>
+              <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              placeholder="usuario@bq.com"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {touched.email && errors.email && <div className='error'>{errors.email}</div>}
+            </div>
+            <div>
+              <label htmlFor="password">Contraseña</label>
+              <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              placeholder="******"
+              value={values.placeholder}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              />
+              {touched.password && errors.password && <div className='error'>{errors.password}</div>}
+            </div>
+            <button type="submit">Ingresar</button>
+          </form>
+        )}
+      </Formik>
+		</div>
+    
+	);
 }
+
+export default Formulario;
