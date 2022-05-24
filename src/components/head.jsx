3cb3ecd4129510324da2005b094682624a6858c0
AnamariaMC <anymedinac14@gmail.com>
2022-05-24 10:18:30 -0500

    Revisión de cambio de vistas con links y se agrega Outlet

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import logo from '../lib/logo.png'
 
 export default function Head(props) {
   return (
+    <div>
     <nav className='head'>
       <img className='logo' src={logo} alt="logo" />      
-        <NavLink to='' style={{color:"#f1f1f1"}} activeClassName='active' >{props.text1}</NavLink>
-        <NavLink to='' style={{color:"#f1f1f1"}} activeClassName='active' >{props.text2}</NavLink>       
+        <Link to='/waiter/order' style={{color:"#f1f1f1"}} activeClassName='active' >{props.text1}</Link>
+        <Link to='/waiter/list' style={{color:"#f1f1f1"}} activeClassName='active' >{props.text2}</Link>       
     </nav>
+    <section>
+      <Outlet />
+    </section>
+    </div>
   )
 }
 
