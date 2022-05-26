573b97a0be073904c410f45674072724794da5b1
AnamariaMC <anymedinac14@gmail.com>
2022-05-26 16:21:31 -0500

    se importa CartProvider para ser usado por ProductsMenu y Summary

--- src/componentsWaiter/menu.jsx
+++ src/componentsWaiter/menu.jsx
@@ -1,10 +1,15 @@
+import { useCart } from "react-use-cart";
+
 const Menu = (props) => {
-  const onClick = () => {
-    props.handleAddProduct({name: props.name, price: props.price, id: props.id, type: props.type});
-  }
+  const { addItem } = useCart();
+  // const onClick = () => {
+  //   props.handleAddProduct({name: props.name, price: props.price, id: props.id, type: props.type});
+  // }
 
   return (
-    <div className='product' onClick={onClick}>
+    <div className='product' 
+    onClick={() =>addItem(props.item)}>
+      
       <img src={props.image} alt={props.name} />
       <p>{props.name}</p>
       <p>${props.price}</p>
--- src/componentsWaiter/order.jsx
+++ src/componentsWaiter/order.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProductsMenu from './products'
 import Summary from './summary'
+import { CartProvider } from 'react-use-cart'
 
 
 export default function Order() {
   
-  return (
-    <div> 
+  return (    
+    <> 
+      <CartProvider>
       <div>
         <h1 style={{color:"#f1f1f1"}}>Estas son las ordenes </h1>
       </div>
@@ -16,6 +18,7 @@ export default function Order() {
       <div className='resumen' >
         <Summary />
       </div>
-  </div>
+      </CartProvider> 
+    </>     
   )
 }
