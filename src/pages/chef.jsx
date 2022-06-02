53cf5254c414ed5dcd2d06e037b4f728a8263dd1
Karen Alarcón <akareny.10@gmail.com>
2022-06-02 13:06:43 -0500

    se crean componentes de las vistas del chef

--- src/pages/chef.jsx
+++ src/pages/chef.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { getOrder } from '../orderpetitions';
-import { useEffect, useState} from 'react';
-
+import logo from '../lib/logo.png'
+import SummaryOrders from "../componentsChef/SummaryOrders"
+import SummaryReady from "../componentsChef/SummaryReady"
 
 export default function Chef() {
-  const [order, setOrder] = useState([]); //array de objetos
-  
-  
-  useEffect(() => { // useEffect es una función que se ejecuta cuando el componente se monta
-    getOrder() // llamamos a la función products() que está en el provider
-      .then((response) => { // cuando la función products() se ejecuta, se ejecuta la función then()
-        console.log(response.data);
-        localStorage.setItem("order",(response.data));
-        setOrder(response.data); // guardamos los datos en el estado
-      })
-      .catch(() => {});
-  },[]);
 
   return (
-    <div>
-      <h1 style={{color:"#f1f1f1"}} >Vista del Chef</h1>
-    </div>
-  )
+    <>
+      {/* <section className="{style.container}"> */}
+        <img className='logo' src={logo} alt="logo"/>
+        <div>
+          <h1 style={{color:'#f1f1f1'} }> Vista del Chef </h1>
+          <h2 style= {{color:"#f1f1f1", fontSize: '20px', fontWeight: 'bold', textAlign: 'center'}}> PEDIDOS PENDIENTES </h2>
+          <SummaryOrders/>
+        </div>
+        <div>
+        <SummaryReady/>
+
+        </div>
+        {/* </section> */}
+    </>
+      )
 }
+
+
