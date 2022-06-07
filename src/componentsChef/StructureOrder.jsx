fd6ed2d6e8954d8e1883bc9c9631f9628dc53aa1
AnamariaMC <anymedinac14@gmail.com>
2022-06-06 23:48:10 -0500

    cambio del nombre del parametro al totalOrders para imprimir data

--- src/componentsChef/StructureOrder.jsx
+++ src/componentsChef/StructureOrder.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Items from './Items'
+import '../css/chef.css'
 
-export default function StructureOrder(props) {
+export default function StructureOrder({totalOrders}) {
     return (
         <div>
-           <p>{props.name}</p>
-            <p style={{color:'#FE8D06', fontWeight:'700'}}>{props.client}</p>
-            <p style={{color:'#FE8D06', fontWeight:'700'}}>{props.date}</p>
-            <p style={{color:'#FE8D06', fontWeight:'700'}}>{props.items.status}</p>
-            <Items 
-                products = {props.items.products}
+        <div className='containerOrders'>
+          <div className='nameDate'>
+            <p style={{color:'#FE8D06', fontWeight:'700'}}>{totalOrders.client}</p>
+            <p style={{color:'#FE8D06', fontWeight:'700'}}>{totalOrders.dataEntry}</p>
+          </div>
+          <h5>Pedido:</h5>
+            <Items style={{color:'#FE8D06', fontWeight:'700'}}
+                products = {totalOrders.products}
             />
+            <button className='btnListo' type='submit'>Listo</button>
+            
         </div>
+      </div> 
+        
            )
        
     }
