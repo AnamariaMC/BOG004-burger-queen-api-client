2398985cda2e6bf1c6e512f947cd3a69277d6a88
AnamariaMC <anymedinac14@gmail.com>
2022-06-06 23:41:23 -0500

    Arreglo en el UseEffect para ver pedidos en tiempo real, cambio de return por totalOrders

--- src/componentsChef/SummaryOrders.jsx
+++ src/componentsChef/SummaryOrders.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import { getOrder } from '../orderpetitions';
-// import { useEffect, useState} from 'react';
 import { useEffect, useState } from 'react';
 import { getToken } from '../petitions';
 import StructureOrder from './StructureOrder';
+import '../css/chef.css'
 
 export default function SummaryOrders() {
   const [order, setOrder] = useState([]); //array de objetos
-
   const token = getToken()
-  useEffect(() => { // useEffect es una función que se ejecuta cuando el componente se monta
+
+  const newOrder = () => {
     getOrder(token.accessToken) // llamamos a la función products() que está en el provider
       .then((response) => { // cuando la función products() se ejecuta, se ejecuta la función then()
-        // console.log(response.data);
-        localStorage.setItem("order", (response.data));
         setOrder(response.data); // guardamos los datos en el estado
       })
-      .catch(() => { });
-  }, [token.accessToken, setOrder]);
-// console.log('SOY LA ORDEN', order)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
+  useEffect(() => { // useEffect es una función que se ejecuta cuando el componente se monta
+    newOrder()
+  }, [token.accessToken, setOrder, order]);
+  
+
   return (
-    <div>
+    <div className='containerSummayOrders'>
       {order.map((orders, index) => {
         return(
           <div key ={index}>
             <StructureOrder
-            date = {orders.dataEntry}
-            client = {orders.client}
-            items = {orders}
+            totalOrders = {orders}
          />
          </div>
            )
