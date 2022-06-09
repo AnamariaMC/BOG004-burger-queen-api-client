73627fc800835e883779117ddb284ef6d6538d9c
AnamariaMC <anymedinac14@gmail.com>
2022-06-09 11:11:10 -0500

    se crea el componente de estado para mostrar tanto en mesero como en chef

---
+++ src/componentsChef/componentEstate.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Items from './Items'
+import '../css/chef.css'
+import { StatusOrder } from '../orderpetitions'
+import { getToken } from '../petitions'
+
+
+export default function ComponentEstate({totalOrders}, orderId) {    
+  const token = getToken();
+  
+      StatusOrder(orderId, token.accessToken)
+      .then((response) => {
+          console.log('resuesta de newStatus', response)
+      })
+      .catch((error) => {
+          console.log('Soy Error', error)
+      })
+  
+  return (
+      <div>
+      <div className='containerOrders'>
+        <div className='nameDate'>
+          <p style={{color:'#FE8D06', fontWeight:'700'}}>{totalOrders.client}</p>
+          {/* <p style={{color:'#FE8D06', fontWeight:'700'}}>{totalOrders.dataEntry}</p> */}
+        </div>
+        <h5>Pedido:</h5>
+          <Items style={{color:'#FE8D06', fontWeight:'700'}}
+              products = {totalOrders.products}                
+              qty = {totalOrders.quantity}
+
+          />
+          {/* <button className='btnListo' type='submit' id='btnListo'
+          onClick={() =>newStatus(totalOrders.id)}
+          >Listo</button>
+          <status style={{color:'#FE8D06', fontWeight:'700'}} /> */}
+      </div>
+    </div> 
+      
+         )
+     
+  }
