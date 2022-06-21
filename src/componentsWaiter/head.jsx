77fba9a7306022b4d4c4b762f187dc772e9800db
Karen Alarcón <akareny.10@gmail.com>
2022-06-21 13:10:04 -0500

    se crean comentarios y se elimina codigo innecesario

--- src/componentsWaiter/head.jsx
+++ src/componentsWaiter/head.jsx
@@ -4,13 +4,16 @@ import logo from '../lib/logo.png'
 import { BiLogOut } from 'react-icons/bi'
 import { useNavigate } from 'react-router'
 
-
 export default function Head(props) {
   const navigate = useNavigate();
-      const logOut=()=>{
-        sessionStorage.clear();
-        navigate('/')
-      }
+  
+  // Funcion de cierre de sesión
+  const logOut=()=>{
+    sessionStorage.clear();
+    navigate('/')
+  }
+
+  // Barra de navegacion de la vista del  mesero
   return (
     <div>
     <nav className='head'>
@@ -20,8 +23,9 @@ export default function Head(props) {
         <Link to='/waiter/delivered' style={{color:"#f1f1f1"}} >{props.text3}</Link>
         <BiLogOut style={{color:'#f1f1f1'} } className='iconLogout' onClick={logOut}/>       
     </nav>
+      {/* Seccion donde se imprimir el contenido segun los links  */}
     <section>
-      <Outlet />
+      <Outlet /> 
     </section>
     </div>
   )
